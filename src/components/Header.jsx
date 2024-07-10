d51a8b8795612b028c7105c01c734a3f0e451f6a
schrodingerszanycat <152669825+thebardofavon@users.noreply.github.com>
2024-07-10 16:17:11 +0530

    Completed NavMenu.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,26 @@ const Header = (props) => {
                     <img src="/images/home-icon.svg"alt="home"/>
                     <span>HOME</span>
                 </a>
-                
+                <a>
+                    <img src="/images/search-icon.svg" alt="SEARCH" />
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img src="/images/original-icon.svg" alt="ORIGINALS" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src="/images/movie-icon.svg" alt="MOVIES" />
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src="/images/series-icon.svg" alt="SERIES" />
+                    <span>SERIES</span>
+                </a>
             </NavMenu>
         </Nav>
     )
@@ -47,14 +66,15 @@ const Logo = styled.a`
 const NavMenu = styled.div`
     align-items: center;
     display: flex;
-    flex-flow: row-reverse nowrap;
+    flex-flow: row nowrap;
     height: 100%;
-    justify-content: flex-start;
-    // margin: 0px;
-    // padding: 0px;
-    // position: relative;
-    // margin-right: auto;
-    // margin-left: 25px;
+    justify-content: flex-end;
+    margin: 0px;
+    padding: 0px;
+    position: relative;
+    margin-right: auto;
+    margin-left: 25px;
+    letter-spacing: 0.01rem;
 
     a {
         display: flex;
