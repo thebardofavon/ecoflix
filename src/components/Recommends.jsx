96d0c4a5af1778cbb26917405fac6ad65a65cba0
schrodingerszanycat <152669825+thebardofavon@users.noreply.github.com>
2024-07-13 18:14:54 +0530

    Created recommended section

---
+++ src/components/Recommends.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Recommends = (props) => {
+    return (
+        <Container>
+            <h4>Recommended For You</h4>
+            <Content>
+                <dv>1</dv>
+                <dv>1</dv>
+                <dv>1</dv>
+                <dv>1</dv>
+                <dv>1</dv>
+            </Content>
+        </Container>
+    )
+};
+
+export default Recommends;
