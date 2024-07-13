ced106dbcc6881747ea492470218da32c6ef9315
schrodingerszanycat <152669825+thebardofavon@users.noreply.github.com>
2024-07-13 22:58:33 +0530

    Added all movies to firebase

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,55 @@ import Recommends from "./Recommends";
 import NewEcoFlix from "./NewEcoFlix";
 import Originals from "./Originals";
 import Trending from "./Trending";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import db from "../firebase";
+import { setMovies } from "../features/movie/movieSlice";
+import { selectUserName } from "../features/user/userSlice";
 
 const Home = (props) => {
+    const dispatch = useDispatch();
+    const userName = useSelector(selectUserName);
+    let recommends = [];
+    let newEcoFlixes = [];
+    let originals = [];
+    let trending = [];
+
+    useEffect(() => {
+      console.log("hello");
+      db.collection("movies").onSnapshot((snapshot) => {
+        snapshot.docs.map((doc) => {
+          console.log(recommends);
+          switch (doc.data().type) {
+            case "recommend":
+              recommends = [...recommends, { id: doc.id, ...doc.data() }];
+              break;
+
+            case "new":
+              newEcoFlixes = [...newEcoFlixes, { id: doc.id, ...doc.data() }];
+              break;
+
+            case "original":
+              originals = [...originals, { id: doc.id, ...doc.data() }];
+              break;
+
+            case "trending":
+              trending = [...trending, { id: doc.id, ...doc.data() }];
+              break;
+          }
+        });
+
+        dispatch(
+          setMovies({
+            recommend: recommends,
+            newDisney: newEcoFlixes,
+            original: originals,
+            trending: trending,
+          })
+        );
+      });
+    }, [userName]);
+
     return (
         <Container>
           <ImgSlider />
