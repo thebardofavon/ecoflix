11863a7a91b58497be5e581791b73869a2a03231
schrodingerszanycat <152669825+thebardofavon@users.noreply.github.com>
2024-07-14 12:07:53 +0530

    Working on detail page

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -11,12 +11,34 @@ const Detail = (props) => {
                 />
             </Background>
 
-            {/* <ImageTitle>
+            <ImageTitle>
                 <img 
                     src="https://plus.unsplash.com/premium_photo-1676637000058-96549206fe71?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
                     alt="" 
-                />
-            </ImageTitle> */}
+                /> 
+            </ImageTitle>
+
+            <ContentMeta>
+                <Controls>
+                    <Player>
+                        <img src="/images/play-icon-black.png" alt=""/>
+                        <span>Play</span>
+                    </Player>
+                    <Trailer>
+                        <img src="/images/play-icon-white.png" alt=""/>
+                        <span>Trailer</span>
+                    </Trailer>
+                    <AddList>
+                        <span />
+                        <span />
+                    </AddList>
+                    <GroupWatch>
+                        <div>
+                            <img src="/images/group-icon.png" alt="" />
+                        </div>
+                    </GroupWatch>
+                </Controls>
+            </ContentMeta>
 
         </Container>
     )
@@ -48,4 +70,131 @@ const Background = styled.div`
         }
     }
 `;
+
+const ImageTitle = styled.div`
+    align-items: flex-end;
+    display: flex;
+    -webkit-box-pack: start;
+    justify-content: flex-start;
+    margin: 0px auto;
+    height: 30vw;
+    min-height: 170px;
+    padding-bottom: 24px;
+    width: 100%;
+
+    img {
+        max-width: 600px;
+        min-width: 200px;
+        width: 35vw;
+    }
+`;
+
+const ContentMeta = styled.div`
+    max-width: 874px;
+`;
+
+const Controls = styled.div`
+    align-items: center;
+    display: flex;
+    flex-flow: row nowrap;
+    margin: 24px 0px;
+    min-height: 56px;
+`;
+
+const Player = styled.button`
+    font-size: 15px;
+    margin: 0px 22px 0px 0px;
+    padding: 0px 24px;
+    height: 56px;
+    border-radius: 4px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    letter-spacing: 1.8px;
+    text-align: center;
+    text-transform: uppercase;
+    background: rgb (249, 249, 249);
+    border: none;
+    color: rgb(0, 0, 0);
+
+    img {
+        width: 32px;
+    }
+
+    &:hover {
+        background: rgb(198, 198, 198);
+    }
+
+    @media (max-width: 768px) {
+        height: 45px;
+        padding: 0px 12px;
+        font-size: 12px;
+        margin: 0px 10px 0px 0px;
+
+        img {
+        width: 25px;
+        }
+    }
+`;
+
+const Trailer = styled(Player)`
+    background: rgba(0, 0, 0, 0.3);
+    border: 1px solid rgb(249, 249, 249);
+    color: rgb(249, 249, 249);
+`;
+
+const AddList = styled.div`
+    margin-right: 16px;
+    height: 44px;
+    width: 44px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.6);
+    border-radius: 50%;
+    border: 2px solid white;
+    cursor: pointer;
+
+    span {
+        background-color: rgb(249, 249, 249);
+        display: inline-block;
+
+        &:first-child {
+        height: 2px;
+        transform: translateX(8px) rotate(0deg);
+        width: 16px;
+        }
+
+        &:nth-child(2) {
+        height: 2px;
+        transform: translateX(-8px) rotate(90deg);
+        width: 16px;
+        }
+    }
+`;
+
+const GroupWatch = styled.div`
+    height: 44px;
+    width: 44px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    background: white;
+
+    div {
+        height: 40px;
+        width: 40px;
+        background: rgb(0, 0, 0);
+        border-radius: 50%;
+
+        img {
+            width: 100%;
+        }
+    }
+`;
+
+
 export default Detail;
